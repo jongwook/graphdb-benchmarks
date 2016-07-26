42312896ebc4ca0d30394855bbbf800670c70fa5
Jong Wook Kim <jongwook@nyu.edu>
2016-07-26 17:41:20 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 
 JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
 
-if [ $JAVA_VER -le 18 ] && command -v jabba > /dev/null 2>&1; then jabba use 1.8; fi
+if [ $JAVA_VER -lt 18 ]; then
+  if command -v jabba > /dev/null 2>&1; then 
+    jabba use 1.8; 
+  else
+    echo "Java >= 1.8 required"
+    exit -1
+  fi
+fi
 export JAVA_HOME
 
 export MAVEN_OPTS="-Xmx16g"
